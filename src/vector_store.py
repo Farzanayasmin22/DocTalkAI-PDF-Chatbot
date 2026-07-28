ee8f2821aae40eb992f8b3aeae48850bcb2d6500
Farzanayasmin22 <yasminfarzana983@gmail.com>
2026-07-28 17:21:45 +0530

    Update vector_store.py

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,1 +1,19 @@
+"""
+vector_store.py
 
+This module creates the ChromaDB
+vector database for semantic search.
+"""
+
+from langchain_chroma import Chroma
+
+
+def create_vector_store(chunks, embedding_model, persist_directory):
+    """
+    Store document embeddings in ChromaDB.
+    """
+    return Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_directory
+    )
