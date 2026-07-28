eec3cd1885d1b69ef70c40e3f4a6d3b7c0cea08c
Farzanayasmin22 <yasminfarzana983@gmail.com>
2026-07-28 17:22:33 +0530

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -1,1 +1,28 @@
+"""
+llm.py
 
+This module loads the Gemini
+Large Language Model.
+"""
+
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+def get_llm():
+    """
+    Load the Gemini model.
+    """
+    load_dotenv()
+
+    api_key = os.getenv("GOOGLE_API_KEY")
+
+    if not api_key:
+        raise ValueError("GOOGLE_API_KEY not found.")
+
+    return ChatGoogleGenerativeAI(
+        model="gemini-flash-latest",
+        google_api_key=api_key,
+        temperature=0
+    )
